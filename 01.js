9f69f5b0caf9b738bb72bd387fe9030c2fc9135c
Ferney Jerez <ferney9212@gmail.com>
2020-02-03 21:23:21 -0500

    1. Install Vue js with CDM

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({   
+    el:'#app',
+    data:{
+        titulo:'Este es un saludo',
+        frutas: [
+            {nombre:'Pera',cantidad:10},
+            {nombre:'Manzana',cantidad:1},
+            {nombre:'Plantano',cantidad:15},
+        ]
+    }
+})
