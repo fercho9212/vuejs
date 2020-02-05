f1df2f953df0ac692b9e159f3bc84d8fb26934ae
Ferney Jerez <ferney9212@gmail.com>
2020-02-04 20:58:12 -0500

    3. v-model

--- 01.js
+++ 01.js
@@ -6,6 +6,12 @@ const app = new Vue({
             {nombre:'Pera',cantidad:10},
             {nombre:'Manzana',cantidad:1},
             {nombre:'Plantano',cantidad:15},
-        ]
+        ],
+        nuevaFruta: ''
+    },
+    methods:{
+        guargar () {
+            this.frutas.push({nombre:this.nuevaFruta,cantidad:10});
+        }
     }
 })
